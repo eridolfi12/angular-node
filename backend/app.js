e87cd4ed63a45395c50ccabafe2d1442783fba1a
Humanity's last hope <eridolfi@pbi.org>
2019-07-18 14:06:31 -0400

    Created endpoint for fetching initial posts

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('This is the first middleware');
-  next();
-});
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {id: 0, title: 'Post from Server', content: 'This is coming from the server!'},
+    {id: 1, title: 'Post from Server', content: 'This is coming from the server!'},
+    {id: 2, title: 'Post from Server', content: 'This is coming from the server!'},
+    {id: 3, title: 'Post from Server', content: 'This is coming from the server!'},
+  ];
 
-app.use((req, res, next) => {
-  res.send('Hello from the server!')
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    data: posts
+  });
 });
 
 module.exports = app;
